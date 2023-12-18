957a5bf5b781ca185873de2a53f05aebdb6e3834
benpops89 <me@benpoppy.dev>
2023-12-18 10:24:55 +0000

    day 5: refactor for part 2

--- 2023/05/main.go
+++ 2023/05/main.go
@@ -12,17 +12,12 @@ type Range struct {
 	dst, src, rng int
 }
 
-type Ranges = []Range
-type Steps = []Ranges
-
-func Convert(start int, dst int, src int, rng int) int {
-	fmt.Println("\t\t", start, dst, src, rng)
-	if start >= src && start < src+rng {
-		return dst + (start - src)
-	}
-
-	return start
+type SeedRange struct {
+	start, end int
 }
+
+type Ranges = []Range
+type Maps = []Ranges
 
 func StrToNumSlice(s string) []int {
 	var numbers []int
@@ -40,32 +35,36 @@ func main() {
 	f, _ := os.ReadFile(filename)
 	data := strings.Split(string(f), "\n")
 
-	seeds := StrToNumSlice(data[0][6:])
+	var seeds []SeedRange
+	for _, seed := range StrToNumSlice(data[0][6:]) {
+		seeds = append(seeds, SeedRange{seed, seed})
+	}
 
-	var step = 0
-	var steps Steps = Steps{Ranges{}}
+	var part = 0
+	var maps Maps = Maps{Ranges{}}
 
 	for i := 3; i < len(data); i++ {
 		if data[i] == "" {
 			// End of section update
-			step++
-			steps = append(steps, Ranges{})
+			part++
+			maps = append(maps, Ranges{})
 			i++
 		} else {
 			nums := StrToNumSlice(data[i])
-			steps[step] = append(steps[step], Range{nums[0], nums[1], nums[2]})
+			maps[part] = append(maps[part], Range{nums[0], nums[1], nums[2]})
 		}
 	}
 
-	seed_map := make(map[int]int)
+	seed_map := make(map[SeedRange]SeedRange)
 
 	for _, seed := range seeds {
 		seed_map[seed] = seed
-		for _, ranges := range steps {
+		for _, ranges := range maps {
 			current := seed_map[seed]
 			for _, r := range ranges {
-				if current >= r.src && current < r.src+r.rng {
-					seed_map[seed] = r.dst + current - r.src
+				if current.start >= r.src && current.start < r.src+r.rng {
+					start := r.dst + current.start - r.src
+					seed_map[seed] = SeedRange{start, start}
 					break
 				}
 			}
@@ -74,9 +73,30 @@ func main() {
 
 	p1 := math.MaxInt
 	for _, s := range seed_map {
-		p1 = min(p1, s)
+		p1 = min(p1, s.start)
 	}
 
+	// seed_source := make(map[SeedRange]SeedRange)
+	//
+	// for i := 0; i < len(seeds); i += 2 {
+	// 	seed := SeedRange{seeds[i], seeds[i] + seeds[i+1]}
+	// 	seed_source[seed] = seed
+	// 	// var seedRange = Range{0, seeds[i], seeds[i+1]}
+	// 	// var sourceRange = []Range{seedRange}
+	// 	for _, ranges := range steps {
+	// 		current := seed_source[seed]
+	// 		for _, r := range ranges {
+	// 			start := max(r.src, current.start)
+	// 			end := min(r.src+r.rng, current.end)
+	// 			offset := r.dst - r.src
+	// 			if end > start {
+	// 				seed_source[seed] = SeedRange{seed.start + offset, seed.end + offset}
+	// 			}
+	// 		}
+	// 	}
+	// }
+	//
+	// fmt.Println(seed_source)
 	fmt.Println(seed_map)
 	fmt.Println(p1)
 }
