406fc4069157d042f27826d6a689cd3dcf0bcf01
benpops89 <me@benpoppy.dev>
2023-12-19 11:52:18 +0000

    day 5: solved

--- 2023/05/main.go
+++ 2023/05/main.go
@@ -2,24 +2,94 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-type Range struct {
-	dst, src, rng int
-}
-
-type SeedRange struct {
+type Seed struct {
 	start, end int
 }
 
-type Ranges = []Range
-type Maps = []Ranges
+type Seeds []Seed
 
-func StrToNumSlice(s string) []int {
+type Range struct {
+	dst, src, length int
+}
+
+func main() {
+	filename := os.Args[1]
+
+	f, _ := os.ReadFile(filename)
+	data := strings.Split(strings.TrimSpace(string(f)), "\n\n")
+
+	numbers := strToNum(data[0][6:])
+
+	var p1 []Seed
+	var p2 []Seed
+	for i := 0; i < len(numbers); i += 2 {
+		p1 = append(p1, []Seed{{numbers[i], numbers[i] + 1}, {numbers[i+1], numbers[i+1] + 1}}...)
+		p2 = append(p2, Seed{numbers[i], numbers[i] + numbers[i+1]})
+	}
+
+	parts := [][]Seed{p1, p2}
+	for _, part := range parts {
+		locations := GetLocations(data, part)
+
+		sort.Slice(locations, func(i, j int) bool {
+			return locations[i].start < locations[j].start
+		})
+
+		fmt.Println(locations[0].start)
+	}
+}
+
+func GetLocations(s []string, seeds []Seed) []Seed {
+	var seed Seed
+
+	for _, section := range s[1:] {
+		var ranges []Range
+
+		for _, line := range strings.Split(section, "\n")[1:] {
+			v := strToNum(line)
+			ranges = append(ranges, Range{v[0], v[1], v[2]})
+		}
+
+		var locations []Seed
+		var check bool
+		for len(seeds) > 0 {
+			seed, seeds = seeds[0], seeds[1:]
+			for _, rng := range ranges {
+				check = false
+				start := max(seed.start, rng.src)
+				end := min(seed.end, rng.src+rng.length)
+
+				if start < end {
+					locations = append(locations, Seed{start - rng.src + rng.dst, end - rng.src + rng.dst})
+
+					if start > seed.start {
+						seeds = append(seeds, Seed{seed.start, start})
+					}
+
+					if seed.end > end {
+						seeds = append(seeds, Seed{end, seed.end})
+					}
+					check = true
+					break
+				}
+			}
+			if !check {
+				locations = append(locations, seed)
+			}
+		}
+		seeds = locations
+	}
+
+	return seeds
+}
+
+func strToNum(s string) []int {
 	var numbers []int
 	for _, i := range strings.Fields(strings.TrimSpace(s)) {
 		n, _ := strconv.Atoi(i)
@@ -27,80 +97,4 @@ func StrToNumSlice(s string) []int {
 	}
 
 	return numbers
-}
-
-func main() {
-	filename := os.Args[1]
-
-	f, _ := os.ReadFile(filename)
-	data := strings.Split(string(f), "\n")
-
-	var seeds []SeedRange
-	for _, seed := range StrToNumSlice(data[0][6:]) {
-		seeds = append(seeds, SeedRange{seed, seed})
-	}
-
-	var part = 0
-	var maps Maps = Maps{Ranges{}}
-
-	for i := 3; i < len(data); i++ {
-		if data[i] == "" {
-			// End of section update
-			part++
-			maps = append(maps, Ranges{})
-			i++
-		} else {
-			nums := StrToNumSlice(data[i])
-			maps[part] = append(maps[part], Range{nums[0], nums[1], nums[2]})
-		}
-	}
-
-	seed_map := make(map[SeedRange][]SeedRange)
-
-	for _, seed := range seeds {
-		seed_map[seed] = append(seed_map[seed], seed)
-		for _, ranges := range maps {
-			for _, seed_range := range seed_map[seed] {
-				// current := seed_map[seed]
-				for _, r := range ranges {
-					if seed_range.start >= r.src && seed_range.start < r.src+r.rng {
-						start := r.dst + seed_range.start - r.src
-						seed_map[seed][0] = SeedRange{start, start}
-						break
-					}
-				}
-			}
-		}
-	}
-
-	p1 := math.MaxInt
-	for _, s := range seed_map {
-		for _, sr := range s {
-			p1 = min(p1, sr.start)
-		}
-	}
-
-	// seed_source := make(map[SeedRange]SeedRange)
-	//
-	// for i := 0; i < len(seeds); i += 2 {
-	// 	seed := SeedRange{seeds[i], seeds[i] + seeds[i+1]}
-	// 	seed_source[seed] = seed
-	// 	// var seedRange = Range{0, seeds[i], seeds[i+1]}
-	// 	// var sourceRange = []Range{seedRange}
-	// 	for _, ranges := range steps {
-	// 		current := seed_source[seed]
-	// 		for _, r := range ranges {
-	// 			start := max(r.src, current.start)
-	// 			end := min(r.src+r.rng, current.end)
-	// 			offset := r.dst - r.src
-	// 			if end > start {
-	// 				seed_source[seed] = SeedRange{seed.start + offset, seed.end + offset}
-	// 			}
-	// 		}
-	// 	}
-	// }
-	//
-	// fmt.Println(seed_source)
-	// fmt.Println(seed_map)
-	fmt.Println(p1)
 }
