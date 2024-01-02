27ed8e0c5ecebbce776ab5b6e75de2e293128980
benpop89 <me@benpoppy.dev>
2024-01-02 16:40:32 +0000

    chore: reset solve back to template

--- pkg/aoc/solve.go
+++ pkg/aoc/solve.go
@@ -10,5 +10,4 @@ func Solve(filename string) {
 	file, _ := os.ReadFile(filename)
 	data := strings.Split(strings.TrimSpace(string(file)), "\n")
 	fmt.Println(data)
-	fmt.Println("Add additional information")
 }
